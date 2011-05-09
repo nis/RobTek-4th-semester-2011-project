c3858449f65799d52bbc5892dc49d8317214d5c9
Nis Sarup <nis@sarup.dk>
2011-05-09 13:56:24 +0200

    Added signed int printer.

--- src/lcd/lcd.c
+++ src/lcd/lcd.c
@@ -48,6 +48,16 @@ void write_2_char_int_to_buffer (INT8U start, INT8U line, INT8U i )
 	lcd_add_char_to_buffer(start + 1, line, c2);
 }
 
+void write_3_char_signed_int_to_buffer (INT8U start, INT8U line, INT8S i )
+{
+	if(i < 0)
+	{
+		lcd_add_char_to_buffer(start, line, '-');
+	}
+	
+	write_3_char_int_to_buffer (start + 1, line, (INT8U) i*(-1) );
+}
+
 void write_3_char_int_to_buffer (INT8U start, INT8U line, INT8U i )
 {
 	INT8U c1 = int_to_ascii((i / 100) % 10);
